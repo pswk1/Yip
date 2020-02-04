06bb9612f10cb8eb866faacb9eb752fc1c09d5f1
Armadillo <ajknapp2015@gmail.com>
2020-02-04 13:04:09 -0800

    Created example api request to zomato

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //5b9c13f9e30c6d6bcd91ac54f9a8bf91
+
+//"user-key: 5b9c13f9e30c6d6bcd91ac54f9a8bf91" "https://developers.zomato.com/api/v2.1/cities?q=Turlock"
+
+$.ajax({
+    url: "https://developers.zomato.com/api/v2.1/search?entity_id=Los%20Angeles&entity_type=city&q=Taco",
+    method: "GET",
+    headers: {
+        "user-key" : "5b9c13f9e30c6d6bcd91ac54f9a8bf91"
+    }
+}).then(function(response) {
+    console.log(response);
+})
